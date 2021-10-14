a9edad44f8d16c824b8730bb219183d02b23c476
Alisson Youssf <youssfbr@gmail.com>
2021-10-14 11:15:28 -0300

    Migration

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MeuTodo.Models
+﻿namespace MeuTodo.Models
 {
     public class Todo
-    {
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
     }
 }
