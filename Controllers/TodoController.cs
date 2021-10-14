65ecc9a711f43837bd6a61be0d7658b7c38c9065
Alisson Youssf <youssfbr@gmail.com>
2021-10-14 11:57:06 -0300

    Get Test

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MeuTodo.Models;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,14 +7,12 @@ using Microsoft.Extensions.Hosting;
 namespace MeuTodo
 {
     public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    {        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+                
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,10 +24,9 @@ namespace MeuTodo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");                
             });
         }
     }
